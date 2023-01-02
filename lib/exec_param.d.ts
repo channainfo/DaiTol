1658037d4423700f029e4ae1ce9a275cf8e5847f
Channa Ly <channa.info@gmail.com>
2023-01-02 21:30:06 +0700

    Use github action to publish

--- lib/exec_param.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class ExecParam extends Map<string, any> {
-    static from(options: Map<string, any>): ExecParam;
-}
--- lib/exec_result.d.ts
+++
@@ -1,8 +0,0 @@
-import { ExecParam } from "./exec_param";
-export declare class ExecResult extends ExecParam {
-    failed: boolean;
-    fail(message: string): void;
-    failByMessage(message: string): void;
-    errorMessage(): string | null;
-    isSuccess(): boolean;
-}
